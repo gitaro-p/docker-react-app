7e13c0a847bc767270783dc845a07052e86e3ede
清川　裕太郎 <yuu7353@gmail.com>
2020-08-01 19:34:39 +0900

    refを使ったユーザー入力の取得

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 
 import TodoList from './components/TodoList';
+import NewTodo from './components/NewTodo';
 
 // function App() {
 //   return <div className="App"></div>;
 // }
 
 const App: React.FC = () => {
-  const todos = [{ id: 't1', text: 'TypeScriptコースの完了' }];
+  const todos = [{ id: 't1', text: 'React+TypeScript' }];
   return (
     <div className="App">
+      <NewTodo />
       <TodoList items={todos} />
     </div>
   );
